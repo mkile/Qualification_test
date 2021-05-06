3626ca0bfd0d45ed3d15102e4f8a9b05e6cf2df8
mkile <mkiles81@gmail.com>
2021-05-06 23:12:33 +0300

    fixed files

--- tests/test_opencart_find_elements.py
+++ tests/test_opencart_find_elements.py
@@ -6,96 +6,18 @@ from selenium.webdriver.support import expected_conditions
 DELAY = 2
 
 
-@pytest.mark.parametrize("element", ['#slideshow0',
-                                     '#carousel0',
-                                     '#menu',
-                                     '#top-links',
-                                     '#cart',
-                                     '#search'])
-def test_index_elements(browser, url, element):
-    browser.get(url)
-    try:
-        req_element = WebDriverWait(browser, DELAY).until(
-            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, element))
-        )
-    except Exception as Err:
-        req_element = False
-    else:
-        req_element = True
-    assert req_element is True, element
-
-
-@pytest.mark.parametrize("element", ['#top',
-                                     '#menu',
-                                     '.breadcrumb',
-                                     '#column-left',
-                                     '.swiper-viewport',
-                                     '.product-thumb'])
-def test_products_page_elements(browser, url, element):
-    browser.get(url + '/index.php?route=product/category&path=20')
-    try:
-        req_element = WebDriverWait(browser, DELAY).until(
-            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, element))
-        )
-    except Exception as Err:
-        req_element = False
-    else:
-        req_element = True
-    assert req_element is True, element
-
-
-@pytest.mark.parametrize("element", ['.thumbnails',
-                                     '.image-additional',
-                                     'button.btn-primary',
-                                     'div.tab-content',
-                                     'span.fa.fa-stack',
-                                     'a.addthis_button_facebook_like'])
-def test_product_page_elements(browser, url, element):
-    browser.get(url + '/index.php?route=product/product&path=57&product_id=49')
-    try:
-        req_element = WebDriverWait(browser, DELAY).until(
-            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, element))
-        )
-    except Exception as Err:
-        req_element = False
-    else:
-        req_element = True
-    assert req_element is True, element
-
-
-@pytest.mark.parametrize("element", ['a.btn.btn-primary',
-                                     'input.btn.btn-primary',
-                                     'label.control-label[for=input-email]',
-                                     'label.control-label[for=input-password]',
-                                     '#menu',
-                                     '#cart'])
-def test_login_page_elements(browser, url, element):
-    browser.get(url + '/index.php?route=account/login')
-    try:
-        req_element = WebDriverWait(browser, DELAY).until(
-            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, element))
-        )
-    except Exception as Err:
-        req_element = False
-    else:
-        req_element = True
-    assert req_element is True, element
-
-
-@pytest.mark.parametrize("element", ['h1.panel-title',
-                                     '#input-username',
-                                     '#input-password',
-                                     'span.help-block',
-                                     'button.btn-primary',
-                                     '#header-logo'])
-def test_admin_page_elements(browser, url, element):
-    browser.get(url + '/admin')
-    try:
-        req_element = WebDriverWait(browser, DELAY).until(
-            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, element))
-        )
-    except Exception as Err:
-        req_element = False
-    else:
-        req_element = True
-    assert req_element is True, element
+# @pytest.mark.parametrize("page", range(5))
+# @pytest.mark.parametrize("element", range(5))
+# def test_admin_page_elements(browser, page, element, test_parameters, url):
+#     keys = list(test_parameters.keys())
+#     current_page = test_parameters[keys[page]]
+#     browser.get(url + keys[page])
+#     try:
+#         req_element = WebDriverWait(browser, DELAY).until(
+#             expected_conditions.presence_of_element_located((By.CSS_SELECTOR, current_page[element]))
+#         )
+#     except Exception as Err:
+#         req_element = False
+#     else:
+#         req_element = True
+#     assert req_element is True
