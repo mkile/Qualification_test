38cf1cc626f0ddc4449358d2788b94b74c5d4d03
Mikhail <mkiles81@gmail.com>
2021-05-06 23:15:12 +0300

    Update test_opencart_find_elements.py

--- tests/test_opencart_find_elements.py
+++ tests/test_opencart_find_elements.py
@@ -6,18 +6,18 @@ from selenium.webdriver.support import expected_conditions
 DELAY = 2
 
 
-# @pytest.mark.parametrize("page", range(5))
-# @pytest.mark.parametrize("element", range(5))
-# def test_admin_page_elements(browser, page, element, test_parameters, url):
-#     keys = list(test_parameters.keys())
-#     current_page = test_parameters[keys[page]]
-#     browser.get(url + keys[page])
-#     try:
-#         req_element = WebDriverWait(browser, DELAY).until(
-#             expected_conditions.presence_of_element_located((By.CSS_SELECTOR, current_page[element]))
-#         )
-#     except Exception as Err:
-#         req_element = False
-#     else:
-#         req_element = True
-#     assert req_element is True
+@pytest.mark.parametrize("page", range(5))
+@pytest.mark.parametrize("element", range(5))
+def test_admin_page_elements(browser, page, element, test_parameters, url):
+    keys = list(test_parameters.keys())
+    current_page = test_parameters[keys[page]]
+    browser.get(url + keys[page])
+    try:
+        req_element = WebDriverWait(browser, DELAY).until(
+            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, current_page[element]))
+        )
+    except Exception as Err:
+        req_element = False
+    else:
+        req_element = True
+    assert req_element is True
