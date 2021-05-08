3c518047e36bacd832d72c5fb087695aecaea9b6
Mikhail <mkiles81@gmail.com>
2021-05-08 20:18:36 +0300

    Update test_opencart_find_elements.py

--- tests/test_opencart_find_elements.py
+++ tests/test_opencart_find_elements.py
@@ -2,6 +2,7 @@ import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import TimeoutException
 
 DELAY = 2
 
