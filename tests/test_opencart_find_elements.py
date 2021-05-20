dc145e24a6beeb9236c0026da9eceab64d43c5be
Михаил Овсянкин <mkiles81@gmail.com>
2021-05-20 18:00:12 +0300

    Fixed json. TODO: rewrite page_objects, add tests from hw part 2

--- tests/test_opencart_find_elements.py
+++ tests/test_opencart_find_elements.py
@@ -1,10 +1,5 @@
 import pytest
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.common.exceptions import TimeoutException
-
-DELAY = 2
+from page_objects.GenericPage import GenericPage
 
 
 @pytest.mark.parametrize("page", range(5))
@@ -13,11 +8,4 @@ def test_admin_page_elements(browser, page, element, test_parameters, url):
     keys = list(test_parameters.keys())
     current_page = test_parameters[keys[page]]
     browser.get(url + keys[page])
-    try:
-        WebDriverWait(browser, DELAY).until(
-            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, current_page[element]))
-        )
-    except TimeoutException as Err:
-        raise AssertionError("На странице {} не найден элемент {}".format(keys[page], current_page[element]))
-    else:
-        assert True
+    GenericPage(browser).wait_element(current_page[element])
