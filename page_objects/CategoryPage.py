7d009016b347bb7a9f3e9104f01cdd2630158749
Mikhail <mkiles81@gmail.com>
2021-05-31 18:40:10 +0300

    Update CategoryPage.py

--- page_objects/CategoryPage.py
+++ page_objects/CategoryPage.py
@@ -3,9 +3,9 @@ from .MainPage import MainPage
 
 
 class CategoryPage(MainPage):
-    TABLET_SELECTOR = (By.CSS_SELECTOR,
+    ELEMENT_SELECTOR = (By.CSS_SELECTOR,
                        '#content > div:nth-child(3) > div > div > div:nth-child(2) > div.caption > h4 > a')
 
     def open_tablet_product(self):
-        element = self._verify_element_presence(self.TABLET_SELECTOR)
+        element = self._verify_element_presence(self.ELEMENT_SELECTOR)
         self._click_element(element)
