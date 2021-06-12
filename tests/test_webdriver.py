3e3c22793fc8ba7fa0093d36e39ca8573b9a335d
Mikhail <mkiles81@gmail.com>
2021-06-12 17:54:48 +0300

    Update test_webdriver.py

--- tests/test_webdriver.py
+++ tests/test_webdriver.py
@@ -7,5 +7,4 @@ def test_title_value(browser, url):
     allure.dynamic.title(f'Проверка работы браузера при открытии ссылки {url}')
     allure.step(f'Open {url}')
     browser.get(url)
-    current_title = match(r'.+o|Opencart.+', browser.title)
-    assert current_title
+    assert match(r'.+o|Opencart.+', browser.title)
